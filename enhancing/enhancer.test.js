086d8799126eedb8333d9fc5e09127283e05c942
Gordon Caister <gordoncaister@gmail.com>
2020-05-19 12:30:00 -0500

    created test for failure, first test working

--- enhancing/enhancer.test.js
+++ enhancing/enhancer.test.js
@@ -24,4 +24,16 @@ describe("Test for enhancer", () =>{
             expect(actual.durability).toBe(sword.durability)
         })
     })
+    describe("Test for failure",()=>{
+        it("Enhancement is less than 15, durabilty decreased by 5",() =>{
+            const actual = enhancer.fail({...sword,enhancement:14})
+            expect(actual.durability).toBe(sword.durability-5)
+        })
+        // it("Enhancement is less than 15, durabilty decreased by 5",() =>{
+            
+        // })
+        // it("Enhancement is less than 15, durabilty decreased by 5",() =>{
+            
+        // })
+    })
 })
